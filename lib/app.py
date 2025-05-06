d92cce8a63173c2320c84bd12a624363d0961ed4
Joan Nyawira Kori <korijoan171@gmail.com>
2025-05-06 12:39:02 +0300

    Print usage

---
+++ lib/app.py
@@ -0,0 +1,7 @@
+# lib/app.py
+print("Hello World!", end=" ") # must add space between the quotation marks
+print("Pass this test,", end=" ")
+print("please.", end="\n")
+
+#The "end" parameter ensures that a new line is not created,,,\n is for creating a new line
+
